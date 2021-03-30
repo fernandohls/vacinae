0dc5b9de23ba05af2f23f97a49bd78064f935cdb
Fernando Henrique Laranjo Silva <fernandohenrique@laranjosilva.com>
2021-03-30 20:54:30 -0300

    Criando projeto.

---
+++ src/main/java/com/adft/vacinae/VacinaeApplication.java
@@ -0,0 +1,13 @@
+package com.adft.vacinae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adft/vacinae/VacinaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adft.vacinae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
